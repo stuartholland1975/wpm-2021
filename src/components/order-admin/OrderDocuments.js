0a48574e5773fc32f1ab99567406bddfd4b1f724
stuar tholland <stuartgholland@gmail.com>
2021-09-22 02:52:57 +0100

    netlify commit

--- src/components/order-admin/OrderDocuments.js
+++ src/components/order-admin/OrderDocuments.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {gql, useQuery} from '@apollo/client';
 import {Document, Page, pdfjs} from 'react-pdf/dist/esm/entry.webpack';
 import {useParams} from 'react-router-dom';
-import {Box, Card, CardActionArea, CardContent, CircularProgress,} from '@mui/material';
+import {Box, Card, CardActionArea, CardContent, CircularProgress} from '@mui/material';
 import Typography from '@mui/material/Typography';
 import OrderDocumentButtons from "../button-bars/OrderDocumentButtons";
 
@@ -25,65 +25,77 @@ const GET_ORDER_DOCUMENTS = gql`
 `;
 
 const DocumentDisplay = (data) => (
-    <Card
-        sx={{
-            bgcolor: '#d0ddea',
-            p: 2,
-            ml: 1, mr: 1,
-            maxWidth: 378
-        }}
-        key={data.document.id}
-    >
-        <CardActionArea
-            href={`https://workpm.ddns.net/documents/${data.document.headerDocumentFile.id}`}
-            target='_blank'
+  <Box
+
+    xs={'auto'}
+    key={data.document.id}
+  >
+    <Card sx={{
+      bgcolor: '#d0ddea',
+      height: 600,
+      // p: 2,
+      ml: 1, mr: 1,
+
+    }}>
+      <CardActionArea
+        href={`https://workpm.ddns.net/documents/${data.document.headerDocumentFile.id}`}
+        target='_blank'
+      >
+        <Typography
+          sx={{
+            textDecoration: 'underline',
+            mb: 1,
+            color: 'navy',
+            textTransform: 'uppercase',
+            fontSize: 18,
+            maxHeight: 500
+          }}>{data.document.title}</Typography>
+
+
+        <CardContent><Document
+
+          file={{url: `https://workpm.ddns.net/documents/${data.document.headerDocumentFile.id}`}}
         >
-            <Typography
-                sx={{
-                    textDecoration: 'underline',
-                    mb: 1,
-                    color: 'navy',
-                    textTransform: 'uppercase',
-                    fontSize: 18
-                }}>{data.document.title}</Typography>
+          <Page pageNumber={1} width={300}/>
+        </Document></CardContent>
 
 
-            <CardContent><Document
-
-                file={{url: `https://workpm.ddns.net/documents/${data.document.headerDocumentFile.id}`}}
-            >
-                <Page pageNumber={1} width={350}/>
-            </Document></CardContent>
-
-
-        </CardActionArea>
+      </CardActionArea>
 
     </Card>
+  </Box>
 );
 
 const OrderDocuments = () => {
-    const {orderId} = useParams();
-    const {data, loading} = useQuery(GET_ORDER_DOCUMENTS, {
-        variables: {orderId: Number(orderId)},
-        fetchPolicy: 'network-only',
-    });
-    if (loading) return <CircularProgress/>;
+  const {orderId} = useParams();
+  const {data, loading} = useQuery(GET_ORDER_DOCUMENTS, {
+    variables: {orderId: Number(orderId)},
+    fetchPolicy: 'network-only',
+  });
+  if (loading) return <CircularProgress/>;
 
-    return (<>
-        <OrderDocumentButtons/>
-        <Box sx={{height: 600, display: 'flex', mt: 5, mr: 1, ml: 1, justifyContent: 'center'}}>
+  return (<>
+    <OrderDocumentButtons/>
+    {/* <Box sx={{
+      height: 600,
+      alignContent: 'center',
+      display: 'grid',
+      gridTemplateColumns:
+        '      minmax(100px, 1fr)\n' +
+        '      repeat(6, 1fr)'
+    }}>*/}
+    <Box sx={{display: 'grid', gridTemplateColumns: 'repeat(6, 1fr)', mb: 2, height: 600}}>
+      {data.orderheaderDocuments.nodes.length > 0 ? (
 
-            {data.orderheaderDocuments.nodes.length > 0 ? (
+        data.orderheaderDocuments.nodes.map((item) => DocumentDisplay(item))
+      ) : (
+        <div className='no-data-message'>
+          NO DOCUMENTS ARE CURRENLTY ASSOCIATED WITH THIS ORDER
+        </div>)}
 
-                data.orderheaderDocuments.nodes.map((item) => DocumentDisplay(item))
-            ) : (
-                <div className='no-data-message'>
-                    NO DOCUMENTS ARE CURRENLTY ASSOCIATED WITH THIS ORDER
-                </div>)}
+    </Box>
 
-        </Box>
-
-    </>)
+  </>)
 };
 
 export default OrderDocuments;
--- src/components/ui-components/pdf-viewer/PdfViewer.js
+++ src/components/ui-components/pdf-viewer/PdfViewer.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const PdfViewer = (props) => {
+    const [thumb, setThumb] = React.useState()
+
+    React.useEffect(() => {
+
+    }, [])
     return (
-        <div>PDF VIEWER</div>
+      <div>PDF VIEWER</div>
     )
 }
 
