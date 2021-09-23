39ef52a1f477c243b0d9b30a04fb072dc21fdda6
stuar tholland <stuartgholland@gmail.com>
2021-09-23 18:23:41 +0100

    netlify commit

--- src/components/order-admin/OrderAdmin.js
+++ src/components/order-admin/OrderAdmin.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import OrderAdminButtons from '../button-bars/OrderAdminButtons';
 import OrderStats from './OrderStats';
 import {Route, Switch} from 'react-router-dom';
-import OrderLocations from './OrderLocations';
-import OrderItems from './OrderItems';
-import OrderImages from './OrderImages';
-import OrderDocuments from "./OrderDocuments";
+import {CircularProgress} from "@mui/material";
+
+const OrderLocations = lazy(() => import('./OrderLocations'));
+const OrderItems = lazy(() => import('./OrderItems'));
+const OrderImages = lazy(() => import('./OrderImages'));
+const OrderDocuments = lazy(() => import('./OrderDocuments'));
 
 const OrderAdmin = () => {
   return (
@@ -14,20 +16,22 @@ const OrderAdmin = () => {
       <OrderAdminButtons/>
       <OrderStats/>
       <hr/>
-      <Switch>
-        <Route path='/orders/admin/locations/:orderId' exact>
-          <OrderLocations/>
-        </Route>
-        <Route path='/orders/admin/items/:orderId' exact>
-          <OrderItems/>
-        </Route>
-        <Route path='/orders/admin/images/:orderId' exact>
-          <OrderImages/>
-        </Route>
-        <Route path='/orders/admin/documents/:orderId' exact>
-          <OrderDocuments/>
-        </Route>
-      </Switch>
+      <Suspense fallback={<CircularProgress/>}>
+        <Switch>
+          <Route path='/orders/admin/locations/:orderId' exact>
+            <OrderLocations/>
+          </Route>
+          <Route path='/orders/admin/items/:orderId' exact>
+            <OrderItems/>
+          </Route>
+          <Route path='/orders/admin/images/:orderId' exact>
+            <OrderImages/>
+          </Route>
+          <Route path='/orders/admin/documents/:orderId' exact>
+            <OrderDocuments/>
+          </Route>
+        </Switch>
+      </Suspense>
     </>
   );
 };
